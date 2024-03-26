5a012226c19b52101a6f35830b4d2740568bd068
Pedro Sawczuk <pedrosawczuk53@gmail.com>
2024-03-26 16:47:40 -0400

    1.1 - Estilizando Botões

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -21,29 +21,146 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 30,
-          ),
-          ElevatedButton.icon(
-            onPressed: () {
-              Navigator.pushNamed(context, AppRoutes.registrarPessoaPage);
-            },
-            icon: Icon(Icons.person_add),
-            label: Text('Adicionar Pessoas'),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          ElevatedButton.icon(
-            onPressed: () {
-              Navigator.pushNamed(context, AppRoutes.listarPessoaPage);
-            },
-            icon: Icon(Icons.person),
-            label: Text('Listar Pessoas'),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+
+            // botão registrar pessoas
+            ElevatedButton.icon(
+              onPressed: () {
+                Navigator.pushNamed(context, AppRoutes.registrarPessoaPage);
+              },
+              icon: Icon(
+                Icons.person_add,
+                color: Colors.white,
+              ),
+              label: Text(
+                'Adicionar Pessoas',
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.indigoAccent.shade400,
+                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+
+            // botão listar pessoas
+            ElevatedButton.icon(
+              onPressed: () {
+                Navigator.pushNamed(context, AppRoutes.listarPessoaPage);
+              },
+              icon: Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
+              label: Text(
+                'Listar Pessoas',
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.indigoAccent.shade400,
+                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+              ),
+            ),
+
+            SizedBox(
+              height: 10,
+            ),
+
+            // botão registrar Produtos
+            ElevatedButton.icon(
+              onPressed: () {
+                // Navigator.pushNamed(context, AppRoutes.registrarPessoaPage);
+              },
+              icon: Icon(
+                Icons.shopping_cart_rounded,
+                color: Colors.white,
+              ),
+              label: Text(
+                'Adicionar Produtos',
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.indigoAccent.shade400,
+                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+              ),
+            ),
+
+            SizedBox(
+              height: 10,
+            ),
+
+            // botão listar Produtos
+            ElevatedButton.icon(
+              onPressed: () {
+                // Navigator.pushNamed(context, AppRoutes.listarPessoaPage);
+              },
+              icon: Icon(
+                Icons.add_shopping_cart,
+                color: Colors.white,
+              ),
+              label: Text(
+                'Listar Produtos',
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.indigoAccent.shade400,
+                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+
+            // botão registrar Fornecedores
+            ElevatedButton.icon(
+              onPressed: () {
+                // Navigator.pushNamed(context, AppRoutes.registrarPessoaPage);
+              },
+              icon: Icon(
+                Icons.other_houses_rounded,
+                color: Colors.white,
+              ),
+              label: Text(
+                'Adicionar Fornecedores',
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.indigoAccent.shade400,
+                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+              ),
+            ),
+
+            SizedBox(
+              height: 10,
+            ),
+
+            // botão listar Fornecedores
+            ElevatedButton.icon(
+              onPressed: () {
+                // Navigator.pushNamed(context, AppRoutes.listarPessoaPage);
+              },
+              icon: Icon(
+                Icons.credit_card_outlined,
+                color: Colors.white,
+              ),
+              label: Text(
+                'Listar Fornecedores',
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.indigoAccent.shade400,
+                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
